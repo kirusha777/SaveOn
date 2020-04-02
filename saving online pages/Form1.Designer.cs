26eeca32c24fa7d72a2b831b140783bea71d46f4
kodnokurtsev <kodnokurtsev@mail.ru>
2020-04-02 18:54:53 +0300

    1

---
+++ saving online pages/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace saving_online_pages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.link = new System.Windows.Forms.TextBox();
+            this.name_link = new System.Windows.Forms.Label();
+            this.save_website = new System.Windows.Forms.Button();
+            this.proxy = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // link
+            // 
+            this.link.Location = new System.Drawing.Point(12, 28);
+            this.link.Name = "link";
+            this.link.Size = new System.Drawing.Size(129, 20);
+            this.link.TabIndex = 0;
+            this.link.Text = "https://cattish.ru/breed/";
+            // 
+            // name_link
+            // 
+            this.name_link.AutoSize = true;
+            this.name_link.Location = new System.Drawing.Point(12, 9);
+            this.name_link.Name = "name_link";
+            this.name_link.Size = new System.Drawing.Size(129, 13);
+            this.name_link.TabIndex = 1;
+            this.name_link.Text = "введите ссылку на сайт";
+            // 
+            // save_website
+            // 
+            this.save_website.Location = new System.Drawing.Point(30, 107);
+            this.save_website.Name = "save_website";
+            this.save_website.Size = new System.Drawing.Size(90, 23);
+            this.save_website.TabIndex = 2;
+            this.save_website.Text = "скачать сайт";
+            this.save_website.UseVisualStyleBackColor = true;
+            this.save_website.Click += new System.EventHandler(this.save_website_Click);
+            // 
+            // proxy
+            // 
+            this.proxy.AutoSize = true;
+            this.proxy.Location = new System.Drawing.Point(12, 70);
+            this.proxy.Name = "proxy";
+            this.proxy.Size = new System.Drawing.Size(136, 17);
+            this.proxy.TabIndex = 3;
+            this.proxy.Text = "использовать прокси";
+            this.proxy.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(156, 166);
+            this.Controls.Add(this.proxy);
+            this.Controls.Add(this.save_website);
+            this.Controls.Add(this.name_link);
+            this.Controls.Add(this.link);
+            this.Name = "Form1";
+            this.Text = "saving online pages";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox link;
+        private System.Windows.Forms.Label name_link;
+        private System.Windows.Forms.Button save_website;
+        private System.Windows.Forms.CheckBox proxy;
+    }
+}
+
---
+++ saving online pages/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace saving_online_pages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        public const int KEYEVENTF_KEYDOWN = 0x0000; // New definition
+        public const int KEYEVENTF_EXTENDEDKEY = 0x0001; //нажатие
+        public const int KEYEVENTF_KEYUP = 0x0002; //отпускание
+        public const int VK_LCONTROL = 0xA2; //код ctrl
+        public const int S = 0x53; //код S
+        public const int ENTER = 0x0D;//код Enter
+
+        public static void PressKeys()
+        {
+            keybd_event(VK_LCONTROL, 0, KEYEVENTF_KEYDOWN, 0);
+            keybd_event(S, 0, KEYEVENTF_KEYDOWN, 0);
+            keybd_event(S, 0, KEYEVENTF_KEYUP, 0);
+            keybd_event(VK_LCONTROL, 0, KEYEVENTF_KEYUP, 0);
+        }
+        public static void Enter()
+        {
+            keybd_event(ENTER, 0, KEYEVENTF_KEYDOWN, 0);
+            keybd_event(ENTER, 0, KEYEVENTF_KEYUP, 0);
+        }
+        private void save_website_Click(object sender, EventArgs e)
+        {
+            var website = link.Text;
+            System.Diagnostics.Process.Start(website);
+            Thread.Sleep(5000);
+            PressKeys();
+            Thread.Sleep(1000);
+            Enter();
+        }
+
+    }
+}
